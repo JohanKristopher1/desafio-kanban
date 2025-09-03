9a90d01483cbdd187e7bdc6fd9f2bc271e467567
JohanKristopher <113453715+JohanKristopher@users.noreply.github.com>
2025-09-03 14:45:34 -0400

    API integration

--- src/components/DraggableComponents.vue
+++ src/components/DraggableComponents.vue
@@ -1,59 +1,36 @@
 <template>
   <v-row>
         <v-col>
-          <v-btn prepend-icon="mdi-filter-variant" color="#7C3AED" width="160px" height="60px">Filtra</v-btn>
+          <v-btn prepend-icon="mdi-filter-variant" color="#7C3AED" style="width: 160px; height: 60px;">Filtra</v-btn>
         </v-col>
         <v-col cols="11">       
           <v-text-field prepend-inner-icon="mdi-magnify" label="Busque por Titulo..." variant="solo-filled"  height="48px"></v-text-field>
         </v-col>
   </v-row>
   <v-row display="flex" justify="space-around" align="center">
-      <v-col cols="4">
-      <h3>A fazer</h3>
-      <draggable class="list-group" :list="list1" group="people" @change="log" item-key="name">
-        <template #item="{ element, index }">
-            <div class="list-group-item">
-                {{ element.name }} {{ index }}
-            </div>
+      <v-col cols="4"
+      v-for="(col, index) in colum"
+      :key="index"
+      >
+      <h2>{{ col.name }}</h2>
+      <draggable  v-model="col.items" group="tasks" item-key="id">
+        <template #item="{ element }">
+          <div class="task">
+            {{ element.title }}
+          </div>
         </template>
       </draggable>
       </v-col>
-
-    <v-col cols="4">
-      <h3>Fazendo</h3>
-      <draggable class="list-group" :list="list2" group="people" @change="log" item-key="name">
-        <template #item="{ element, index }">
-            <div class="list-group-item">
-                {{ element.name }} {{ index }}
-            </div>
-        </template>
-      </draggable>
-    </v-col>
-
-    <v-col cols="4">
-      <h3>Feito</h3>
-      <draggable class="list-group" :list="list3" group="people" @change="log" item-key="name">
-        <template #item="{ element, index }">
-            <div class="list-group-item">
-                {{ element.name }} {{ index }}
-            </div>
-        </template>
-      </draggable>
-    </v-col>
-
-    <rawDisplayer class="col-3" :value="list1" title="List 1" />
-
-    <rawDisplayer class="col-3" :value="list2" title="List 2" />
-
-    <rawDisplayer class="col-3" :value="list3" title="List 3" />
   </v-row>
 </template>
+
 <script lang="ts">
 import draggable from "vuedraggable";
 import tasksApiService from "@/services/tasks/tasks-api-service";
 import { typeCard } from "@/types/card-types";
+import { defineComponent } from "vue";
 
-export default {
+export default defineComponent({
   name: "two-lists",
   display: "Two Lists",
   order: 1,
@@ -62,27 +39,27 @@ export default {
   },
   data() {
     return {
+      colum: [] as any[],
       tasks: [] as typeCard[],
       task: {} as typeCard
     };
   },
   async created() {
+    this.colum = [
+      { name: "A fazer", items: [] },
+      { name: "Fazendo", items: [] },
+      { name: "Feito", items: [] }
+    ];
     await tasksApiService.getAll().then((lista: typeCard[]) => {
       this.tasks = lista;
-      console.log(this.tasks);
-    }).catch((error: typeCard) => {
+      this.colum[0].items = this.tasks.filter((task:typeCard) => task.status === 'todo');
+      this.colum[1].items = this.tasks.filter((task:typeCard) => task.status === 'doing');
+      this.colum[2].items = this.tasks.filter((task:typeCard) => task.status === 'done');
+    }).catch((error: any) => {
       console.error(`Erro: ${error.message}`);
     }).finally(() => {
       console.log('Requisição finalizada');
     });
   },
-  watch: {
-    async tasks(novoValor) {
-      console.log('Tasks atualizadas:', novoValor[0].id);
-
-      const tarefas = await tasksApiService.getId(novoValor[0].id);
-      this.task = tarefas;
-    }
-  }
-} 
+ })    
 </script>
--- src/types/card-types.ts
+++ src/types/card-types.ts
@@ -1,6 +1,7 @@
 export interface typeCard {
     id: number;
     title: string;
+    status: string;
     description: string;
     label: string;
     message?: string;
